03f3eaed033cefe71005720c35e6c70ba3454617
crru <C.Reuter@t-systems.com>
2020-02-06 06:22:54 +0100

    test

--- src/main/java/com/sirheadless/SimpleApp.java
+++ src/main/java/com/sirheadless/SimpleApp.java
@@ -1,24 +1,28 @@
 package com.sirheadless;
 
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 
+import static spark.Spark.get;
+
 public class SimpleApp {
     public static void main(String[] args) {
-        String logFile = "c:/spark-2.4.4-bin-hadoop2.7/README.md"; // Should be some file on your system
-        SparkConf sparkConf = new SparkConf().setAppName("Simple Application").setMaster("local[*]");
-        SparkContext sparkContext = new SparkContext(sparkConf);
-        SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
-        Dataset<String> logData = spark.read().textFile(logFile).cache();
-
-        long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
-        long numBs = logData.filter((FilterFunction<String>) s -> s.contains("b")).count();
-
-        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
-
-        spark.stop();
+        get("/hello", (request, response) -> "Hello World!");
+//        String logFile = "/tmp/README.md"; // Should be some file on your system
+//        SparkConf sparkConf = new SparkConf().setAppName("Simple Application").setMaster("local[*]");
+//        SparkContext sparkContext = new SparkContext(sparkConf);
+//        SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+//        Dataset<String> logData = spark.read().textFile(logFile).cache();
+//
+//        long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
+//        long numBs = logData.filter((FilterFunction<String>) s -> s.contains("b")).count();
+//
+//        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+//
+//        spark.stop();
     }
 }
